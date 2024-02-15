44d5b28a87fedf082f68c643a951903b492bd165
Duolikun-Daniya <daniya.d@northeastern.edu>
2024-02-15 17:26:30 -0500

    Add ImpliedReturn component

--- src/labs/a3/JavaScript/variables/WorkingWithFunctions.tsx
+++ src/labs/a3/JavaScript/variables/WorkingWithFunctions.tsx
@@ -1,10 +1,14 @@
 import ES5Functions from "../functions/ES5Functions";
+import ArrowFunctions from "../functions/ArrowFunctions";
+import ImpliedReturn from "../functions/ImpliedReturn";
 
 function WorkingWithFunctions() {
   return (
     <div>
       <h1>Working With Functions</h1>
       <ES5Functions />
+      <ArrowFunctions />
+      <ImpliedReturn />
     </div>
   );
 }
