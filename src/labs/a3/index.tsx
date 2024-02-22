7868e349bfa686d2b43fd1446bb97981c8c82eb9
Duolikun-Daniya <daniya.d@northeastern.edu>
2024-02-21 23:28:45 -0500

    Add conditional output components

--- src/labs/a3/index.tsx
+++ src/labs/a3/index.tsx
@@ -1,4 +1,5 @@
 import Classes from "./Classes";
+import ConditionalOutput from "./ConditionalOutput";
 import JavaScript from "./JavaScript";
 import PathParameters from "./routing/PathParameters";
 import Styles from "./Styles";
@@ -8,6 +9,7 @@ function Assignment3() {
     //* The library was imported in src/index.tsx and the classes are available to all components without needing to import it again. You can also create additional CSS files and import them in TSX files that need specific styling.
     <div id="a3" className="container">
       <h1>Assignment 3</h1>
+      <ConditionalOutput />
       <Styles />
       <Classes />
       <PathParameters />
