5dce5caa0a6b0accb3e0197ccfa03ee3db9f8b6a
Duolikun-Daniya <daniya.d@northeastern.edu>
2024-02-21 19:40:20 -0500

    Add ForLoops component

---
+++ src/labs/a3/JavaScript/arrays/ForLoops.tsx
@@ -0,0 +1,17 @@
+let stringArray1 = ["string1", "string3"];
+let stringArray2: string[] = [];
+for (let i = 0; i < stringArray1.length; i++) {
+  const string1 = stringArray1[i];
+  stringArray2.push(string1.toUpperCase());
+}
+
+function ForLoops() {
+  return (
+    <div>
+      <h2>ForLoops</h2>
+      stringArray2 = {stringArray2} <br />
+    </div>
+  );
+}
+
+export default ForLoops;
