fa747a579fb13b4eb680251004ac962757b34d6c
Duolikun-Daniya <daniya.d@northeastern.edu>
2024-02-21 23:17:24 -0500

    Add Styles component

--- src/labs/a3/index.tsx
+++ src/labs/a3/index.tsx
@@ -1,12 +1,14 @@
 import Classes from "./Classes";
 import JavaScript from "./JavaScript";
 import PathParameters from "./routing/PathParameters";
+import Styles from "./Styles";
 function Assignment3() {
   return (
     //* "container" is a Bootstrap class.
     //* The library was imported in src/index.tsx and the classes are available to all components without needing to import it again. You can also create additional CSS files and import them in TSX files that need specific styling.
     <div id="a3" className="container">
       <h1>Assignment 3</h1>
+      <Styles />
       <Classes />
       <PathParameters />
       <JavaScript />
