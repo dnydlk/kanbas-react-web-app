d778351ee5aeab2714972216cb732bc426e486cc
Duolikun-Daniya <daniya.d@northeastern.edu>
2024-02-21 23:07:45 -0500

    Add Classes component

--- src/labs/a3/index.tsx
+++ src/labs/a3/index.tsx
@@ -1,3 +1,4 @@
+import Classes from "./Classes";
 import JavaScript from "./JavaScript";
 import PathParameters from "./routing/PathParameters";
 function Assignment3() {
@@ -6,6 +7,7 @@ function Assignment3() {
     //* The library was imported in src/index.tsx and the classes are available to all components without needing to import it again. You can also create additional CSS files and import them in TSX files that need specific styling.
     <div id="a3" className="container">
       <h1>Assignment 3</h1>
+      <Classes />
       <PathParameters />
       <JavaScript />
     </div>
