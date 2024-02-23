67124bd42e8cb01f1e08e32a278e728816a94da8
Duolikun-Daniya <daniya.d@northeastern.edu>
2024-02-22 20:01:33 -0500

    Add user, enrollments, and grades data to Database module

--- src/kanbas/Database/index.ts
+++ src/kanbas/Database/index.ts
@@ -1,4 +1,7 @@
 import courses from "./courses.json";
 import modules from "./modules.json";
 import assignments from "./assignments.json";
-export { courses, modules, assignments };
+import user from "./user.json";
+import enrollments from "./enrollments.json";
+import grades from "./grades.json";
+export default { courses, modules, assignments, user, enrollments, grades };
