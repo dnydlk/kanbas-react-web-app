a833bbdee3ed848918e646e0ad2533a74a94e147
dnydlk <dnydlk97@gmail.com>
2024-03-19 23:14:39 -0400

    fix(Course-C-Nav): responsive display

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -4,7 +4,7 @@ function CourseNavigation() {
   const links = ["Home", "Modules", "Piazza", "Grades", "Assignments"];
   const { pathname } = useLocation();
   return (
-    <ul className="wd-course-navigation d-sm-none d-md-block">
+    <ul className="wd-course-navigation d-none d-md-block">
       {links.map((link, index) => (
         <li key={index} className={pathname.includes(link) ? "wd-active" : ""}>
           <Link to={link}>{link}</Link>
